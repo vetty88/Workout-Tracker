a119cf330f5e090d56c7ba1d9afe7a798c5cf43e
Vetty <vetty88@gmail.com>
2020-10-07 21:44:11 +1100

    deploying

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    }
+);
+
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
